77287df939668223b9f13cfd9eb2d852dba666d3
HyoJung Yoon <yhjune09@gmail.com>
2025-09-04 14:33:40 +0900

    setup(performance): add DTO performanceResponse #8

--- src/main/java/org/ddcn41/ticketing_system/dto/response/PerformanceResponse.java
+++ src/main/java/org/ddcn41/ticketing_system/dto/response/PerformanceResponse.java
@@ -4,10 +4,18 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.ddcn41.ticketing_system.entity.Performance;
+
+import java.math.BigDecimal;
 
 @Builder
 @Getter
 @AllArgsConstructor
-@NoArgsConstructor
 public class PerformanceResponse {
+    private String title;
+    private String venue;
+    private String theme;
+    private String posterUrl;
+    private BigDecimal price;
+    private Performance.PerformanceStatus status;
 }
