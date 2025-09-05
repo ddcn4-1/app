52188f934bc2900ef850712e588cf7a26e830dd5
Hwara <hwara1221@gmail.com>
2025-09-05 10:24:27 +0900

    feat(admin): service default setting #18

--- src/main/java/org/ddcn41/ticketing_system/service/AdminService.java
+++ src/main/java/org/ddcn41/ticketing_system/service/AdminService.java
@@ -1,7 +1,11 @@
 package org.ddcn41.ticketing_system.service;
 
+import lombok.RequiredArgsConstructor;
 import org.ddcn41.ticketing_system.dto.DashboardDto;
+import org.springframework.stereotype.Service;
 
+@Service
+@RequiredArgsConstructor
 public class AdminService {
 
     public DashboardDto getDashboardData() {
